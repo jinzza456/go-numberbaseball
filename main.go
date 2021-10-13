b6bc45235679e7e0edda2fb3883360152a639829
jinzza456 <jinzza456@gmail.com>
2021-10-13 16:36:10 +0900

    rand number

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math/rand" // 랜덤 숫자를 위한 패키지 호출
+	"time"      //항상 변하는 seed 값을 주기위해 time을 호출
 )
 
 func main() {
-	numbers := MakeNumbers() // 무작위 숫자 3개를 만든다.
+	rand.Seed(time.Now().UnixNano()) // 랜덤 숫자를 위해 seed값 설정
+	numbers := MakeNumbers()         // 무작위 숫자 3개를 만든다.
 
 	cnt := 0
 
@@ -27,6 +30,25 @@ func main() {
 
 func MakeNumbers() [3]int {
 	var rst [3]int
+
+	for i := 0; i < 3; i++ {
+		for {
+			n := rand.Intn(10)
+			duplicated := false // 겹치는지 안겹치는지에 대한 플래그 변수 초기값
+			for j := 0; j < i; j++ {
+				if rst[j] == n { //숫자가 겹친다면 다시 뽑는다.
+					duplicated = true //겹치면 true
+					break
+				}
+			}
+			if !duplicated { //겹치지 않는다면 for문을 빠져 나간다.
+				rst[i] = n
+				break
+			}
+		}
+	}
+
+	fmt.Println(rst)
 	return rst
 } // 0~9 사이의 겹치지 않는 무작위 숫자 3개를 반환
 
